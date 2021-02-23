ef4696691e35c9a19110c05128f795ea85e952e6
ajeetkumarv <ajeet.innovation@gmail.com>
2021-02-24 01:10:00 +0530

    notes

--- src/main/java/functionalprogramming/StreamsTest.java
+++ src/main/java/functionalprogramming/StreamsTest.java
@@ -13,10 +13,18 @@ public class StreamsTest {
 
         int total1 = imperativeWay(data);
         int total2 = functionalWay(data);
+        int total3 = functionalWay2(data);
 
         System.out.println("Total 1: " + total1);
         System.out.println("Total 2: " + total2);
+        System.out.println("Total 3: " + total3);
 
+        //you can create parallel stream in two ways
+        // 1. data.parallelStream()
+        // 2. data.stream().parallel()
+        // Foundit here: https://www.youtube.com/watch?v=15X0qFtBqiQ&list=WL&index=1
+        // Functional Programming with Java 8 by Venkat Subramaniam
+            
 
     }
 
@@ -46,5 +54,11 @@ public class StreamsTest {
             .reduce(0, (c, e) -> c + e);
     }
 
+    private static int functionalWay2(List<Integer> data) {
+        return data
+            .stream()
+            .mapToInt(e -> e * 2) // gives you IntStream
+            .sum();
+    }
     
 }
